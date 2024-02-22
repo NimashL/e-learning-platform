eda07dd0922e6da135ce97447630fff36749d311
NimashL <lakshithanimash2@gmail.com>
2024-02-22 13:57:21 +0530

    3rd

---
+++ src/main/java/com/example/campus/domain/Course.java
@@ -0,0 +1,47 @@
+package com.example.campus.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "course")
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.MERGE)
+    @JoinColumn(name = "instructor_id", nullable = false)
+    public User user;
+
+    @Column(name = "duration")
+    private String duration;
+
+    @Column(name = "course_fee")
+    private Decim courseFee;
+
+    @Column(name = "created_date")
+    private Date createdDate;
+
+    @Column(name = "created_user")
+    private String createdUser;
+
+    @Column(name = "modified_date")
+    private Date modifiedDate;
+
+    @Column(name = "modified_user")
+    private String modifiedUser;
+
+
+}
